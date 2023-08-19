aa491bd619a3764edea0a3bf5002026476d1ad19
Omar Elnagdy <omarelnagdy16@gmail.com>
2023-08-18 20:36:10 -0400

    added functionality so that only a specific number of guests are allowed

--- src/Components/Attendance.jsx
+++ src/Components/Attendance.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, Typography, IconButton, Grid  } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, Typography, IconButton, Grid } from '@mui/material';
 import DeleteIcon from "@mui/icons-material/Delete";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 import './components.scss';
 import emailjs from 'emailjs-com';
+import GuestList from '../GuestList/GuestList.js'; // Import the GuestList from the separate file
 
 const AttendanceComponent = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [guestName, setGuestName] = useState('');
-  const [guests, setGuests] = useState([]);
+  const [mainGuestName, setMainGuestName] = useState('');
+  const [additionalGuestNames, setAdditionalGuestNames] = useState([]);
   const [phoneNumber, setPhoneNumber] = useState('');
   const [privateMessage, setPrivateMessage] = useState('');
   const [emailSent, setEmailSent] = useState(false);
-
+  const [isMainGuestValid, setIsMainGuestValid] = useState(false); // New state for main guest validity
 
   const openDialog = () => {
-    // Clear existing guest list and reset input fields
-    setGuests([]);
-    setGuestName('');
+    setMainGuestName('');
+    setAdditionalGuestNames([]);
     setPhoneNumber('');
     setPrivateMessage('');
+    setIsMainGuestValid(false); // Clear main guest validity
+    setEmailSent(false);
     setIsDialogOpen(true);
   };
 
@@ -32,11 +36,13 @@ const AttendanceComponent = () => {
     const userId = "6dq3cOnzFTDbaZOC7";
 
     const templateParams = {
-      guestName: guests.join(', '),
-      numPeople: guests.length,
-      phoneNumber: phoneNumber, // Add the phone number field
-      privateMessage: privateMessage, // Add the private message field
+      guestName: [mainGuestName, ...additionalGuestNames].join(', '),
+      numPeople: 1 + additionalGuestNames.length,
+      phoneNumber: phoneNumber,
+      privateMessage: privateMessage,
     };
+
+    // Sending email using emailjs
 
     emailjs
       .send(serviceId, templateId, templateParams, userId)
@@ -50,28 +56,26 @@ const AttendanceComponent = () => {
   };
 
   const handleAddGuest = () => {
-    if (guestName.trim() !== '') {
-      setGuests([...guests, guestName.trim()]);
-      setGuestName('');
+    if (isMainGuestValid && additionalGuestNames.length < GuestList[mainGuestName.toLowerCase()]) {
+      setAdditionalGuestNames([...additionalGuestNames, ""]);
     }
   };
 
   const handleRemoveGuest = (index) => {
-    const updatedGuests = [...guests];
+    const updatedGuests = [...additionalGuestNames];
     updatedGuests.splice(index, 1);
-    setGuests(updatedGuests);
+    setAdditionalGuestNames(updatedGuests);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Call the RSVP submission function here
-    handleRSVPSubmit();
-    closeDialog();
+    if (isMainGuestValid) {
+      handleRSVPSubmit();
+      closeDialog();
+    }
   };
 
-
-  const isSubmitDisabled = guests.length === 0;
-  
+  const isSubmitDisabled = mainGuestName === '' || additionalGuestNames.some(name => name.trim() === '');
 
   return (
     <div className='attendance-component'>
@@ -86,57 +90,88 @@ const AttendanceComponent = () => {
       <Dialog className='modal' open={isDialogOpen} onClose={closeDialog}>
         <DialogTitle>RSVP</DialogTitle>
         <DialogContent>
-        <Grid container justifyContent="center">
-          <Grid>
-            <div className="form">
-              <TextField
-                label="Guest Name"
-                variant="outlined"
-                value={guestName}
-                onChange={(e) => setGuestName(e.target.value)}
-                fullWidth
-                className="input"
-              />
-              <Button style={{backgroundColor: "green"}}className="add-button" variant="contained" onClick={handleAddGuest} fullWidth>
-                Add Guest
-              </Button>
-              <div className="guest-list">
-                {guests.map((guest, index) => (
-                  <div key={index} className="guest-item">
-                    <Typography variant="body1">{guest}</Typography>
-                    <IconButton
-                      aria-label="remove"
-                      color="secondary"
-                      onClick={() => handleRemoveGuest(index)}
-                    >
-                      <DeleteIcon style={{fill: "red"}}/>
-                    </IconButton>
-                  </div>
-                ))}
+          <Grid container justifyContent="center">
+            <Grid>
+              <div className="form">
+                <div className='verify-main-guest-container'>
+                <TextField
+                  label="Main Guest Name"
+                  placeholder='Enter your first and last name'
+                  variant="outlined"
+                  value={mainGuestName}
+                  onChange={(e) => {
+                    const name = e.target.value;
+                    setMainGuestName(name);
+                    setIsMainGuestValid(GuestList.hasOwnProperty(name.toLowerCase())); // Convert to lowercase
+                  }}
+                  fullWidth
+                  className="input"
+                />
+                {/* Display the check or X icon based on validity */}
+                <div className="verify-main-guest-icon">
+                {isMainGuestValid ? <CheckIcon style={{ fill: "green`" }} /> : <ClearIcon style={{ fill: "red" }} />}
+                </div>
+                </div>
+                {isMainGuestValid && (
+                  <Typography variant="caption">
+                    Allowed Guests: {GuestList[mainGuestName.toLowerCase()]}
+                  </Typography>
+                )}
+                <Button
+                  className="add-button"
+                  variant="contained"
+                  onClick={handleAddGuest}
+                  fullWidth
+                  disabled={!isMainGuestValid} // Disable if main guest is not valid
+                >
+                  Add Guest
+                </Button>
+                <div className="guest-list">
+                  {additionalGuestNames.map((guest, index) => (
+                    <div key={index} className="guest-item">
+                      <TextField
+                        label={`Additional Guest #${index + 1}`}
+                        variant="outlined"
+                        value={guest}
+                        onChange={(e) => {
+                          const updatedGuests = [...additionalGuestNames];
+                          updatedGuests[index] = e.target.value;
+                          setAdditionalGuestNames(updatedGuests);
+                        }}
+                        fullWidth
+                        className="input"
+                      />
+                      <IconButton
+                        aria-label="remove"
+                        color="secondary"
+                        onClick={() => handleRemoveGuest(index)}
+                      >
+                        <DeleteIcon style={{ fill: "red" }} />
+                      </IconButton>
+                    </div>
+                  ))}
+                </div>
+                <TextField
+                  label="Phone Number"
+                  variant="outlined"
+                  fullWidth
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="input"
+                />
+                <TextField
+                  label="Private Message"
+                  variant="outlined"
+                  fullWidth
+                  multiline
+                  rows={4}
+                  value={privateMessage}
+                  onChange={(e) => setPrivateMessage(e.target.value)}
+                  className="input"
+                />
               </div>
-              <TextField
-                label="Phone Number"
-                variant="outlined"
-                fullWidth
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                className="input"
-              />
-
-              <TextField
-                label="Private Message"
-                variant="outlined"
-                fullWidth
-                multiline
-                rows={4}
-                value={privateMessage}
-                onChange={(e) => setPrivateMessage(e.target.value)}
-                className="input"
-              />
-
-            </div>
+            </Grid>
           </Grid>
-        </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={closeDialog}>Cancel</Button>
---
+++ src/GuestList/GuestList.js
@@ -0,0 +1,8 @@
+// guestList.js
+const GuestList = {
+    "Mena Elkafafi": 4,
+    "omar elnagdy": 3,
+  };
+  
+  export default GuestList;
+  
