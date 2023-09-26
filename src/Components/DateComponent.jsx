e6b71b18a60430ccd4d667b21479eceea0aad48c
Omar Elnagdy <omarelnagdy16@gmail.com>
2023-09-25 23:47:16 -0400

    fixed calendar

--- src/Components/DateComponent.jsx
+++ src/Components/DateComponent.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
 import { AddToCalendarButton } from 'add-to-calendar-button-react';
-import './components.scss'
+import './components.scss';
 
-const DateComponent = ({ dateTitle, time, dateText, date, startTime, endTime }) => {
-  console.log({dateTitle, time, dateText, date, startTime, endTime})
+
+const DateComponent = ({ dateTitle, time, startTime, endTime }) => {
+
+  const reminder = {
+    method: 'popup', // Use 'popup' for a popup reminder
+    weeks: 1, // Set the reminder to trigger 1 week before the event
+  };
+
   return (
     <div className='date-container'>
-        <div className="component-title">Date</div>
-        <div className="component-text custom-date">
-          <p className='underline'>{dateTitle}</p>
-          <br />
-          <p className='underline'>{time}</p>
-        </div>
-        <AddToCalendarButton
-          name="Omar & Mena's Wedding"
-          startDate={date}
-          startTime={startTime}
-          endTime={endTime}
-          options={['Google','iCal', 'Outlook.com']}
-          className="component-text"
-          />
+      <div className="component-title">Date</div>
+      <div className="component-text custom-date">
+        <p className='underline'>{dateTitle}</p>
+        <br />
+        <p className='underline'>{time}</p>
+      </div>
+      <AddToCalendarButton
+         name="Omar's & Mena's Wedding"
+         startDate="2024-01-21"
+         startTime={startTime}
+         endTime={endTime}
+         timeZone="America/New_York"
+         location="The Tides Estate 1245 Belmont Ave, North Haledon, NJ 07508"
+         description="Omar & Mena's Wedding"
+         hideTextLabelButton
+         options="'Apple','Google','iCal','Outlook.com','Yahoo'"
+         label="Add to Calendar"
+         buttonsList
+         buttonStyle="round"
+         iCalFileName="Reminder-Event"
+         lightMode="bodyScheme"
+         alarm={reminder}
+         rsvp={true}
+      />
     </div>
   );
 };
 
 export default DateComponent;
+
